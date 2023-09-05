04296cd07b59091f12802580a672c15326e6212f
coderjaspreet098 <98249443+coderjaspreet098@users.noreply.github.com>
2023-09-05 14:17:43 +0530

    Update series5.dart

--- series5.dart
+++ series5.dart
@@ -1,130 +1,117 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
+class Imagecropper extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Image Picker with Cropping Example',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: ImagePickerApp(),
-    );
+    return  ImagePickerWidget();
   }
 }
 
-class ImagePickerApp extends StatefulWidget {
+
+class ImagePickerWidget extends StatefulWidget {
   @override
-  _ImagePickerAppState createState() => _ImagePickerAppState();
+  _ImagePickerWidgetState createState() => _ImagePickerWidgetState();
 }
 
-class _ImagePickerAppState extends State<ImagePickerApp> {
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
   File? _image;
-
-  Future getImage(ImageSource source) async {
-    final image = await ImagePicker().pickImage(source: source);
-    if (image == null) return;
-
-    // Crop the selected image
-    File? croppedImage = await cropImage(image.path);
-
-    if (croppedImage == null) return;
-
-    setState(() {
-      this._image = croppedImage;
-    });
+//_getImageFromCamera
+  Future _getImageFromCamera() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image != null) {
+      final croppedImage = await ImageCropper().cropImage(
+        sourcePath: image.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9,
+        ],
+        uiSettings:[ AndroidUiSettings(
+          toolbarTitle: 'Crop Image',
+          toolbarColor: Colors.blue,
+          toolbarWidgetColor: Colors.white,
+          statusBarColor: Colors.blue,
+          backgroundColor: Colors.white,
+        ),
+          IOSUiSettings(
+            title: 'Crop Image',
+            aspectRatioLockEnabled: false,
+          ),],
+      );
+      if (croppedImage != null) {
+        setState(() {
+          _image = File(croppedImage.path);
+        });
+      }
+    }
   }
 
-  Future<File?> cropImage(String imagePath) async {
-    File? croppedFile = await ImageCropper.cropImage(
-      sourcePath: imagePath,
-      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1), // Set your desired aspect ratio
-      compressQuality: 100, // Set the image quality (0 to 100)
-      maxWidth: 800, // Set the maximum width for the cropped image
-      maxHeight: 800, // Set the maximum height for the cropped image
-      androidUiSettings: AndroidUiSettings(
-        toolbarTitle: 'Crop Image',
-        toolbarColor: Colors.teal, // Set the toolbar color
-        toolbarWidgetColor: Colors.white, // Set the toolbar widget color
-        initAspectRatio: CropAspectRatioPreset.square, // Set the initial aspect ratio
-        lockAspectRatio: true, // Lock the aspect ratio
-      ),
-    );
+  Future _getImageFromGallery() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      final croppedImage = await ImageCropper().cropImage(
+        sourcePath: image.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9,
+        ],
+        uiSettings:[ AndroidUiSettings(
+          toolbarTitle: 'Crop Image',
+          toolbarColor: Colors.blue,
+          toolbarWidgetColor: Colors.white,
+          statusBarColor: Colors.blue,
+          backgroundColor: Colors.white,
+        ),
+          IOSUiSettings(
+            title: 'Crop Image',
+            aspectRatioLockEnabled: false,
+          ),],
+      );
 
-    return croppedFile;
+      if (croppedImage != null) {
+        setState(() {
+          _image = File(croppedImage.path);
+        });
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Image Picker with Cropping')),
+          title: Text('Image Picker and Cropper',)
       ),
       body: Center(
         child: Column(
           children: [
-            SizedBox(width: 20,),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                height: 250,
-                width: 250,
-                child: _image != null
-                    ? Image.file(_image!, width: 250, height: 250, fit: BoxFit.cover,)
-                    : Image.network('https://relevant.software/wp-content/uploads/flutter-e1625644676239.png'),
+            if (_image != null)
+              Image.file(
+                _image!,
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
               ),
+            SizedBox(height: 40,),
+            ElevatedButton(
+              onPressed: _getImageFromCamera,
+              child: Text("Pick Image from Camera"),
             ),
-            SizedBox(width: 10,),
-            CustomButton(
-              title: 'Select From Camera',
-              icon: Icons.camera_alt_outlined,
-              onClick: () => getImage(ImageSource.camera),
-            ),
-            SizedBox(width: 10,),
-            CustomButton(
-              title: 'Select From Gallery',
-              icon: Icons.image_outlined,
-              onClick: () => getImage(ImageSource.gallery),
+            ElevatedButton(
+              onPressed: _getImageFromGallery,
+              child: Text("Pick Image from Gallery"),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  final VoidCallback onClick;
-
-  const CustomButton({
-    required this.title,
-    required this.icon,
-    required this.onClick,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      child: ElevatedButton(
-        child: Row(
-          children: [
-            Icon(icon),
-            SizedBox(width: 10,),
-            Text(title),
-          ],
-        ),
-        onPressed: onClick,
       ),
     );
   }

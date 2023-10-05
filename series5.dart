30edf68fa71bce40b5ff3b48ee625d3b10505f49
coderjaspreet098 <98249443+coderjaspreet098@users.noreply.github.com>
2023-10-05 11:08:35 +0530

    Update series5.dart

--- series5.dart
+++ series5.dart
@@ -1,73 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import 'package:untitled/cart_model.dart';
+class MyItem {
+  var id;
+  String? name;
+  int? age;
+  String? otherProperty;
 
+  MyItem({
+    this.id,
+    this.name,
+    this.age,
+    this.otherProperty,
+  });
 
-class Cartapi extends StatefulWidget {
-  const Cartapi({super.key});
-
-  @override
-  State<Cartapi> createState() => _CartapiState();
-}
-
-class _CartapiState extends State<Cartapi> {
-  CartModel?cartModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AppBar Title'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text('API'),
-            ElevatedButton(
-              onPressed: () {
-               cartproducts();
-              },
-              child: Text('click'),
-            ),
-            if (cartModel?.carts != null)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: cartModel!.carts!.length,
-                  itemBuilder: (context, index) {
-                    final product = cartModel!.carts![index];
-                    return Card(
-                      child: ListTile(
-                        title: Column(
-                          children: [
-                            Text('${product.id}'),
-                            Text('${product.products}'),
-                            Text('${product.total}'),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-          ],
-        ),
-      ),
+  factory MyItem.fromJson(Map<String, dynamic> json) {
+    return MyItem(
+      id: json['id'],
+      name: json['name'],
+      age: json['age'],
+      otherProperty: json['otherProperty'],
     );
   }
-
-  void cartproducts() async {
-    print('here');
-    final dio = Dio();
-    final response = await dio.get('https://dummyjson.com/carts');
-    cartModel = CartModel.fromJson(response.data);
-    setState(() {});
-    print(cartModel);
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{
+      'id': id,
+      'name': name,
+      'age': age,
+      'otherProperty': otherProperty,
+    };
+    return data;
   }
 }
-
-
-
-
-
+void main() {
+  final myItem = MyItem(
+    id: 1,
+    name: 'Sample Item',
+    age: 25,
+    otherProperty: 'Some value',
+  );
+  
+  final myItemJson = myItem.toJson();
+  print('MyItem JSON: $myItemJson');
+  
+  final parsedMyItem = MyItem.fromJson({
+    'id': 2,
+    'name': 'Another Item',
+    'age': 30,
+    'otherProperty': 'Another value',
+  });
+  print('Parsed MyItem - ID: ${parsedMyItem.id}, Name: ${parsedMyItem.name}, Age: ${parsedMyItem.age}, Other Property: ${parsedMyItem.otherProperty}');
+}

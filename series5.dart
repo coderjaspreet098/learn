ab092635111d42d941fa093307803397774bc806
coderjaspreet098 <98249443+coderjaspreet098@users.noreply.github.com>
2023-11-01 14:29:40 +0530

    Update series5.dart

--- series5.dart
+++ series5.dart
@@ -1,44 +1,70 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+class CartController extends GetxController {
+  // ... other code ...
 
-
-class MyHomePage9 extends StatefulWidget {
-  @override
-  _MyHomePage9State createState() => _MyHomePage9State();
-}
-
-class _MyHomePage9State extends State<MyHomePage9> {
-  final String apiUrl = 'https://reqres.in/api/users/2';
-
-  Future<void> deleteData() async {
-    final response = await http.delete(
-      Uri.parse(apiUrl),
-      // headers: {
-      //
-      // },
-    );
-
-    if (response.statusCode == 204) {
-      print('DELETE request successful');
-    } else {
-      print('DELETE request failed with status: ${response.statusCode}');
+  Future<void deleteCartData(String id) async {
+    var token = 'token_id';
+    try {
+      var response = await http.delete(
+        Uri.parse('https://dummyjson.com/carts/$id'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      if (response.statusCode == 200) {
+        print(response.body);
+        // Remove the deleted item from the cartList
+        cartList.removeWhere((cart) => cart.carts!.any((c) => c.id == id));
+        update();
+      } else {
+        print('Cart not deleted');
+      }
+    } catch (e) {
+      print(e);
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HTTP DELETE Request Example'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            deleteData();
-          },
-          child: Text('Delete Data'),
-        ),
-      ),
-    );
+  Future<void updateUserData(String id) async {
+    var token = 'token_id';
+    try {
+      var body = {
+        'total': totalController.text,
+        'discountedTotal': discountedtotalController.text,
+        'userId': userIdController.text,
+      };
+
+      var response = await http.put(
+        Uri.parse('https://dummyjson.com/carts/$id'),
+        body: jsonEncode(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        print('Data updated');
+        // You can update the cartList here if needed
+        // Find the cart item and update its values
+        var updatedCart = cartList.firstWhere(
+          (cart) => cart.carts!.any((c) => c.id == id),
+          orElse: () => null,
+        );
+        if (updatedCart != null) {
+          updatedCart.total = totalController.text;
+          updatedCart.discountedTotal = discountedtotalController.text;
+          updatedCart.userId = userIdController.text;
+        }
+        update();
+      } else {
+        print('Response status code not 200');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
+
+  // ... other code ...
 }
